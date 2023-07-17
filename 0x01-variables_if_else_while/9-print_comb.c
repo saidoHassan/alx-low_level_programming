019208c2c4da905c9307602c8c42debfda47f353
saidoHassan <saloonia114@gmail.com>
2023-07-17 15:29:04 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-
-	return (0);
+int num;
+for (num = 0; num <= 9; num++)
+	{
+putchar((num % 10) + '0');
+if (num == 9)
+continue;		
+putchar(',');
+putchar(' ');                  
+}
+putchar('\n');
+return (0);
 }
