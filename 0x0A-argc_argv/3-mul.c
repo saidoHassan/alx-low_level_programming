7df87902d2ac1738e27ff01f4bdf4053edae7c49
saidoHassan <saloonia114@gmail.com>
2023-08-04 13:19:29 -0700

    3-mul.c1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,25 +4,17 @@
 
 /**
  * main - multiplies two numbers.
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 if successful, 1 otherwise
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int res;
-
-	if (argc == 3)
-	{
-		res = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", res);
-		return (0);
-
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
